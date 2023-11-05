0c813e474f95c5cde2ac5b413654752647dc7bbf
shun <shun.0721@hotmail.co.jp>
2023-11-05 05:44:24 +0000

    plot progress

--- make_pair_to_route.py
+++ make_pair_to_route.py
@@ -257,8 +257,9 @@ def make_state_pair_to_state_route(n_states, db_path, latlon_to_state, DG):
 
     partial_process_state_i = functools.partial(process_state_i, states=states, db_path=db_path, latlon_to_state=latlon_to_state, DG=DG)
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        iterator = executor.map(partial_process_state_i, start_states)
-        for _ in iterator:
+        # iterator = executor.map(partial_process_state_i, start_states)
+        futures = [executor.submit(partial_process_state_i, i) for i in start_states]
+        for future in tqdm.tqdm(concurrent.futures.as_completed(futures), total=len(futures)):
             pass
 
 
