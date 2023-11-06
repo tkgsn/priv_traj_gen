e77c1f6b47f3f807517225d32d2739983d12509c
tkgsn <shun.0721@hotmail.co.jp>
2023-11-06 20:29:45 +0000

    eval

--- evaluate.sh
+++ evaluate.sh
@@ -1,3 +1,7 @@
-model_dir=$1
+model_dir=$MODEL_DIR
+location_threshold=$LOCATION_THRESHOLD
+time_threshold=$TIME_THRESHOLD
+n_bins=$N_BINS
+seed=$SEED
 
-python3 evaluation.py --model_dir $model_dir --server
+python3 evaluation.py --model_dir $model_dir --location_threshold $location_threshold --time_threshold $time_threshold --n_bins $n_bins --seed $seed --server
