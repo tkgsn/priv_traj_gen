bbffe6fc35b28d9d09ac9409a38d14db2f03af5e
tkgsn <shun.0721@hotmail.co.jp>
2023-11-07 07:54:08 +0000

    test

--- evaluate.sh
+++ evaluate.sh
@@ -4,4 +4,5 @@ time_threshold=$TIME_THRESHOLD
 n_bins=$N_BINS
 seed=$SEED
 
+echo "hello"
 python3 evaluation.py --model_dir $model_dir --location_threshold $location_threshold --time_threshold $time_threshold --n_bins $n_bins --seed $seed --server
