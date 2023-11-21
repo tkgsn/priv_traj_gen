a9ee1be5b42afc931caf304cc3e2a809863202a7
tkgsn <shun.0721@hotmail.co.jp>
2023-11-21 04:11:35 +0000

    truncate

--- preprocess.sh
+++ preprocess.sh
@@ -27,7 +27,7 @@ data_dir=$(jq -r '.data_dir' config.json)
 data_name=${max_size}
 
 stay_data_name=${location_threshold}_${time_threshold}_bin${n_bins}_seed${seed}
-python3 data_pre_processing.py --dataset ${dataset} --data_name ${data_name} --max_size ${max_size} --seed ${seed} --n_bins $n_bins --time_threshold $time_threshold --location_threshold $location_threshold --save_name ${stay_data_name}
+python3 data_pre_processing.py --dataset ${dataset} --data_name ${data_name} --max_size ${max_size} --seed ${seed} --n_bins $n_bins --time_threshold $time_threshold --location_threshold $location_threshold --save_name ${stay_data_name} --truncate $truncate
 
 route_data_name=0_0_bin${n_bins}_seed${seed}
-python3 data_pre_processing.py --dataset ${dataset} --data_name ${data_name} --max_size ${max_size} --seed ${seed} --n_bins $n_bins --time_threshold 0 --location_threshold 0 --save_name ${route_data_name}
+python3 data_pre_processing.py --dataset ${dataset} --data_name ${data_name} --max_size ${max_size} --seed ${seed} --n_bins $n_bins --time_threshold 0 --location_threshold 0 --save_name ${route_data_name} --truncate $truncate
