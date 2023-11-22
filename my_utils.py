a4dea85f0c165436faa5428c6be37971dced552a
tkgsn <shun.0721@hotmail.co.jp>
2023-11-22 18:47:49 +0000

    test

--- my_utils.py
+++ my_utils.py
@@ -622,7 +622,7 @@ def get(path, parent=False):
         # then download the tar file data.tar.gz in the evaluation-server
         result = subprocess.run(['scp', '-o', 'StrictHostKeyChecking=no', f'evaluation-server:data.tar.gz', destination_file_path])
         # then decompress the tar file to the destination
-        result = subprocess.run(['tar', '-xzf', destination_file_path / "data.tar.gz", "-C", destination_file_path])
+        result = subprocess.run(['tar', '-xzf', destination_file_path / "data.tar.gz"])
     else:
         print('scp', source_file_path, destination_file_path)
         result = subprocess.run(['scp', '-o', 'StrictHostKeyChecking=no', source_file_path, destination_file_path])
