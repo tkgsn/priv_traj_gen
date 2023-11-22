543d31c713317c040a232ab9b1917a9145c17ae2
tkgsn <shun.0721@hotmail.co.jp>
2023-11-22 18:45:23 +0000

    tar

--- my_utils.py
+++ my_utils.py
@@ -618,12 +618,11 @@ def get(path, parent=False):
         print('scp', "-r", source_file_path, destination_file_path)
         # first compose the directory by tar
         # result = subprocess.run(['ssh', '-o', 'StrictHostKeyChecking=no', 'evaluation-server', f"tar -czf data.tar.gz {path}"])
-        result = subprocess.run(['ssh', '-o', 'StrictHostKeyChecking=no', 'evaluation-server', f"tar -czf {path}.tar.gz {path}"])
+        result = subprocess.run(['ssh', '-o', 'StrictHostKeyChecking=no', 'evaluation-server', f"tar -czf data.tar.gz", "-C", f"{source_file_path}"])
         # then download the tar file data.tar.gz in the evaluation-server
-        result = subprocess.run(['scp', '-o', 'StrictHostKeyChecking=no', source_file_path + ".tar.gz", destination_file_path])
+        result = subprocess.run(['scp', '-o', 'StrictHostKeyChecking=no', f'evaluation-server:data.tar.gz', destination_file_path])
         # then decompress the tar file to the destination
-        print('tar', '-xzf', destination_file_path / (directory_name + ".tar.gz"))
-        result = subprocess.run(['tar', '-xzf', destination_file_path / (directory_name + ".tar.gz")])
+        result = subprocess.run(['tar', '-xzf', destination_file_path / "data.tar.gz", "-C", destination_file_path])
     else:
         print('scp', source_file_path, destination_file_path)
         result = subprocess.run(['scp', '-o', 'StrictHostKeyChecking=no', source_file_path, destination_file_path])
