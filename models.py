48b1adb8d923eb3fd065d9551c2e4d941bc734cb
tkgsn <shun.0721@hotmail.co.jp>
2023-11-24 03:57:04 +0000

    small

--- models.py
+++ models.py
@@ -540,7 +540,7 @@ class LinearQuadTreeNetwork(BaseQuadTreeNetwork):
     def __init__(self, n_locations, memory_dim, hidden_dim, n_classes, activate, multilayer=False, is_consistent=False):
         super().__init__(n_locations, memory_dim, hidden_dim, n_classes, activate, is_consistent)
         if multilayer:
-            self.linears = nn.ModuleList([nn.Sequential(nn.Linear(self.memory_dim, 8*self.memory_dim), self.activate, nn.Linear(8*self.memory_dim, 4*self.memory_dim)) for _ in range(self.tree.max_depth)])
+            self.linears = nn.ModuleList([nn.Sequential(nn.Linear(self.memory_dim, self.memory_dim), self.activate, nn.Linear(self.memory_dim, 4*self.memory_dim)) for _ in range(self.tree.max_depth)])
             # self.linears = nn.ModuleList([nn.Sequential(nn.Linear(self.memory_dim, 4*self.memory_dim), self.activate) for _ in range(self.tree.max_depth)])
         else:
             self.linears = nn.ModuleList([nn.Linear(self.memory_dim, 4*self.memory_dim) for _ in range(self.tree.max_depth)])
