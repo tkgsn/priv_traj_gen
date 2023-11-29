00ea516798633728363caaf9fc22398fe5952555
tkgsn <shun.0721@hotmail.co.jp>
2023-11-29 06:20:51 +0000

    ve

--- evaluation.py
+++ evaluation.py
@@ -1059,7 +1059,6 @@ if __name__ == "__main__":
         # get(run_args.model_dir, parent=True)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print("!!!", device)
 
     model_dir = pathlib.Path(run_args.model_dir)
     
@@ -1151,6 +1150,8 @@ if __name__ == "__main__":
             generator, _ = construct_generator(training_dataset.n_locations, meta_network, training_setting["network_type"], training_setting["location_embedding_dim"], training_setting["n_split"], len(training_dataset.label_to_reference), training_setting["hidden_dim"], training_dataset.reference_to_label, logger)
             logger.info(f"evaluate {model_path}")
             generator.load_state_dict(torch.load(model_path, map_location=device))
+        
+        print("??", generator)
         
         args.name = model_path.name
         results = run(generator, dataset, args)
