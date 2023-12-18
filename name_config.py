692c5f61da0554c2af5057c55d0d71e1fb34859a
tkgsn <shun.0721@hotmail.co.jp>
2023-12-18 08:01:05 +0000

    name

--- name_config.py
+++ name_config.py
@@ -10,15 +10,8 @@ def make_save_name(dataset_name, n_bins, time_threshold, location_threshold, see
     return save_name
 
 def make_model_name(**kwargs):
-    # if [ $network_type = "hiemrnet" ]
-    # then
-    #     export save_name=${network_type}_dp${is_dp}_pre${meta_n_iter}_dim${memory_dim}_${memory_hidden_dim}_${location_embedding_dim}_${hidden_dim}_btch${batch_size}_tr${train_all_layers}_co${consistent}
-    # elif [ $network_type = "baseline" ]
-    # then
-    #     export save_name=${network_type}_dp${is_dp}_pre${meta_n_iter}_dim${memory_dim}_${memory_hidden_dim}_${location_embedding_dim}_${hidden_dim}_btch${batch_size}
-    # fi
     if kwargs["network_type"] == "hiemrnet":
-        save_name = f"{kwargs['network_type']}_dp{kwargs['is_dp']}_pre{kwargs['meta_n_iter']}_dim{kwargs['memory_dim']}_{kwargs['memory_hidden_dim']}_{kwargs['location_embedding_dim']}_{kwargs['hidden_dim']}_btch{kwargs['batch_size']}_tr{kwargs['train_all_layers']}_co{kwargs['consistent']}_{kwargs['seed']}"
+        save_name = f"{kwargs['network_type']}_dp{kwargs['is_dp']}_pre{kwargs['meta_n_iter']}_dim{kwargs['memory_dim']}_{kwargs['memory_hidden_dim']}_{kwargs['location_embedding_dim']}_{kwargs['hidden_dim']}_btch{kwargs['batch_size']}_tr{kwargs['train_all_layers']}_co{kwargs['consistent']}_eps{kwargs['epsilon']}_{kwargs['seed']}"
     elif kwargs["network_type"] == "baseline":
         save_name = f"{kwargs['network_type']}_dp{kwargs['is_dp']}_pre{kwargs['meta_n_iter']}_dim{kwargs['memory_dim']}_{kwargs['memory_hidden_dim']}_{kwargs['location_embedding_dim']}_{kwargs['hidden_dim']}_btch{kwargs['batch_size']}_{kwargs['seed']}"
     elif kwargs["network_type"] == "mtnet":
