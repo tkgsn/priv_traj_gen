92cf3d3d517990b1614ef35427f254cc1047af74
tkgsn <shun.0721@hotmail.co.jp>
2023-12-23 13:17:40 +0000

    test

--- run.py
+++ run.py
@@ -309,7 +309,6 @@ def pre_training_meta_network(meta_network, dataset, location_to_class, transiti
         test_input = torch.eye(n_classes).to(device)
         meta_network_output = meta_network(test_input)
         for i in range(n_classes):
-            print(meta_network_output[-1][i].cpu())
             plot_density(torch.exp(meta_network_output[-1][i]).cpu().view(-1), dataset.n_locations, save_dir / "imgs" / f"meta_network_output_{i}.png")
         meta_network.train()
 
