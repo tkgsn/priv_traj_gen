0c8c1c482c6da9fb2a7acae44a1c64817672de3c
tkgsn <shun.0721@hotmail.co.jp>
2023-12-25 02:25:16 +0000

    budgt

--- exps/priv_budget_allocation.py
+++ exps/priv_budget_allocation.py
@@ -24,12 +24,13 @@ location_threshold = 200
 seeds = range(1)
 # meta_dist = "eye"
 meta_dist = "dirichlet"
+transition_type = "first"
 
 def command_hiemrnet(n_bins, epsilon, seed):
     meta_n_iter = 10000
     save_name = make_save_name(dataset, n_bins, time_threshold, location_threshold, seed)
     data_dir = orig_data_dir / dataset / str(max_size) / save_name
-    model_dir = data_dir / make_model_name(network_type="hiemrnet", is_dp=True, meta_n_iter=meta_n_iter, memory_dim=dim, memory_hidden_dim=dim, location_embedding_dim=dim, hidden_dim=dim, batch_size=0, train_all_layers=True, consistent=True, epsilon=epsilon, seed=seed, meta_dist=meta_dist)
+    model_dir = data_dir / make_model_name(network_type="hiemrnet", is_dp=True, meta_n_iter=meta_n_iter, memory_dim=dim, memory_hidden_dim=dim, location_embedding_dim=dim, hidden_dim=dim, batch_size=0, train_all_layers=True, consistent=True, epsilon=epsilon, seed=seed, meta_dist=meta_dist, transition_type=transition_type)
     return f'docker run --rm --gpus all -v /mnt/data:/data -e TRAINING_DATA_DIR={data_dir} -e SEED={seed} -e META_N_ITER={meta_n_iter} -e EPOCH={n_epochs} -e P_BATCH=100 -e DP=True -e MULTI_TASK=True -e CONSISTENT=True -e HIDDEN_DIM={dim} -e LOC_DIM={dim} -e MEM_DIM={dim} -e MEM_HIDDEN_DIM={dim} -e EPSILON={epsilon} -e META_DIST={meta_dist} -e COEF_TIME=1 -e NETWORK_TYPE=hiemrnet kyotohiemrnet.azurecr.io/hiemrnet_cu117 /bin/bash -c "./train.sh"' \
         , f'docker run --rm --gpus all -v /mnt/data:/data -e TEST_THRESH=30 -e ABLATION=True -e SEED=0 -e TRUNCATE=0 -e MODEL_DIR={model_dir} -e EVAL_INTERVAL=10 -e EVAL_DATA_DIR={data_dir} kyotohiemrnet.azurecr.io/hiemrnet_cu117 /bin/bash -c "./evaluate.sh"'
 
