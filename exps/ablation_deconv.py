3d97ceb1f5f7ac18d6832d12de8c1bdfc1e185a7
tkgsn <shun.0721@hotmail.co.jp>
2023-12-31 11:23:14 +0000

    dec

--- exps/ablation_deconv.py
+++ exps/ablation_deconv.py
@@ -18,11 +18,21 @@ seeds = range(10)
 data_dir = orig_data_dir / dataset / str(max_size) / make_save_name(dataset, n_bins, time_threshold, location_threshold, 0)
 
 # conduct each command in parallel with 4 processes
-with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
+with concurrent.futures.ProcessPoolExecutor(max_workers=2) as executor:
 
     for seed in seeds:
             
         for command in [command_baseline(data_dir, dim, seed, epsilon, n_epochs, test_thresh=test_thresh), command_baseline_pre(data_dir, dim, seed, epsilon, n_epochs, test_thresh=test_thresh), command_hiemrnet(data_dir, dim, seed, epsilon, n_epochs, test_thresh=test_thresh), command_hiemrnet_pre(data_dir, dim, seed, epsilon, n_epochs, test_thresh=test_thresh), command_hiemrnet_multitask(data_dir, dim, seed, epsilon, n_epochs, test_thresh=test_thresh), command_hiemrnet_multitask_pre(data_dir, dim, seed, epsilon, n_epochs, test_thresh=test_thresh)]:
-            combined = f"{command[0]}; {command[1]}"
+            combined = f"{command[0]}"
+            print(combined)
+            executor.submit(os.system, combined)
+
+
+with concurrent.futures.ProcessPoolExecutor(max_workers=1) as executor:
+
+    for seed in seeds:
+            
+        for command in [command_baseline(data_dir, dim, seed, epsilon, n_epochs, test_thresh=test_thresh), command_baseline_pre(data_dir, dim, seed, epsilon, n_epochs, test_thresh=test_thresh), command_hiemrnet(data_dir, dim, seed, epsilon, n_epochs, test_thresh=test_thresh), command_hiemrnet_pre(data_dir, dim, seed, epsilon, n_epochs, test_thresh=test_thresh), command_hiemrnet_multitask(data_dir, dim, seed, epsilon, n_epochs, test_thresh=test_thresh), command_hiemrnet_multitask_pre(data_dir, dim, seed, epsilon, n_epochs, test_thresh=test_thresh)]:
+            combined = f"{command[1]}"
             print(combined)
             executor.submit(os.system, combined)
