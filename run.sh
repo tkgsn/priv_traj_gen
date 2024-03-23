6721e12489159010fc7e63faef4099680a9743a8
tkgsn <shun.0721@hotmail.co.jp>
2024-03-23 19:54:57 +0000

    run

--- run.sh
+++ run.sh
@@ -1,17 +1,19 @@
-sudo docker run --rm --gpus all -v ~/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/baseline"
-sudo docker run --rm --gpus all -v ~/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/baseline evaluation_mode=True"
+sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/baseline"
+sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/baseline evaluation_mode=True"
 
-sudo docker run --rm --gpus all -v ~/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/pre"
-sudo docker run --rm --gpus all -v ~/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/pre evaluation_mode=True"
+sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/pre"
+sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/pre evaluation_mode=True"
 
-sudo docker run --rm --gpus all -v ~/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet"
-sudo docker run --rm --gpus all -v ~/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet evaluation_mode=True"
+sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet"
+sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet evaluation_mode=True"
 
-sudo docker run --rm --gpus all -v ~/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_pre"
-sudo docker run --rm --gpus all -v ~/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_pre evaluation_mode=True"
+sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_pre"
+sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_pre evaluation_mode=True"
 
-sudo docker run --rm --gpus all -v ~/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_multitask"
-sudo docker run --rm --gpus all -v ~/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_multitask evaluation_mode=True"
+sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_multitask"
+sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_multitask evaluation_mode=True"
+sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_multitask_cons evaluation_mode=True"
 
-sudo docker run --rm --gpus all -v ~/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_multitask_pre"
-sudo docker run --rm --gpus all -v ~/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_multitask_pre evaluation_mode=True"
+sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_multitask_pre"
+sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_multitask_pre evaluation_mode=True"
+sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_multitask_pre_cons evaluation_mode=True"
