951f9aac50509ea1dd216d2dade044e4f78af634
tkgsn <shun.0721@hotmail.co.jp>
2024-04-03 12:14:19 +0000

    run

--- run.sh
+++ run.sh
@@ -1,19 +1,25 @@
-sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/baseline"
-sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/baseline evaluation_mode=True"
+#!/usr/bin/bash
 
-sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/pre"
-sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/pre evaluation_mode=True"
+dims=(16 32 64 128)
 
-sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet"
-sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet evaluation_mode=True"
+for dim in ${dims[@]}; do
+    sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/baseline location_embedding_dim=$dim memory_hidden_dim=$dim"
+    sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/baseline evaluation_mode=True location_embedding_dim=$dim memory_hidden_dim=$dim"
 
-sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_pre"
-sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_pre evaluation_mode=True"
+    sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/pre location_embedding_dim=$dim memory_hidden_dim=$dim"
+    sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/pre evaluation_mode=True location_embedding_dim=$dim memory_hidden_dim=$dim"
 
-sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_multitask"
-sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_multitask evaluation_mode=True"
-sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_multitask_cons evaluation_mode=True"
+    sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet location_embedding_dim=$dim memory_hidden_dim=$dim"
+    sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet evaluation_mode=True location_embedding_dim=$dim memory_hidden_dim=$dim"
 
-sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_multitask_pre"
-sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_multitask_pre evaluation_mode=True"
-sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_multitask_pre_cons evaluation_mode=True"
+    sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_pre location_embedding_dim=$dim memory_hidden_dim=$dim"
+    sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_pre evaluation_mode=True location_embedding_dim=$dim memory_hidden_dim=$dim"
+
+    sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_multitask location_embedding_dim=$dim memory_hidden_dim=$dim"
+    sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_multitask evaluation_mode=True location_embedding_dim=$dim memory_hidden_dim=$dim"
+    sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_multitask_cons evaluation_mode=True location_embedding_dim=$dim memory_hidden_dim=$dim"
+
+    sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_multitask_pre location_embedding_dim=$dim memory_hidden_dim=$dim"
+    sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_multitask_pre evaluation_mode=True location_embedding_dim=$dim memory_hidden_dim=$dim"
+    sudo docker run --rm --gpus all -v /home/takagi/data:/data hrnet /bin/bash -c "rm requirements.txt && git pull && python3 hub.py +experiment=ablation/hrnet_multitask_pre_cons evaluation_mode=True location_embedding_dim=$dim memory_hidden_dim=$dim"
+done
